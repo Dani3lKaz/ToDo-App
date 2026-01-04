7a61ee8e1648f06bcdd1cde1170997844447ddaa
Daniel <danielkaz2005@gmail.com>
2026-01-04 17:47:07 +0100

    run fix

---
+++ src/main/java/ToDoApp/Launcher.java
@@ -0,0 +1,9 @@
+package ToDoApp;
+
+import javafx.stage.Stage;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/ToDoApp/Main.java
+++ src/main/java/ToDoApp/Main.java
@@ -2,30 +2,28 @@ package ToDoApp;
 
 
 import ToDoApp.db.DataBaseConnection;
-import ToDoApp.model.User;
-import ToDoApp.ui.AccountScreen;
 import ToDoApp.ui.LoginScreen;
-import ToDoApp.utils.Email;
-import ToDoApp.utils.SessionManager;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.sql.Connection;
+import java.util.Objects;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
     public void start(Stage stage) {
         Connection connection = DataBaseConnection.getConnection();
         stage.setTitle("To-Do App");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
         //SessionManager.setCurrentUser(new User(1,"Admin", new Email("admin@mail.com"), "Admin123"));
         //new AccountScreen(connection).show(stage);
         new LoginScreen(connection).show(stage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
